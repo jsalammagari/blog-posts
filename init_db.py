1a83cc6bbfdd6578dc10639efbea2702d6de912f
JahnaviSalammagari <jahnavireddy.salammagari@gmail.com>
2023-09-14 12:07:09 -0700

    update in init_db

--- init_db.py
+++ init_db.py
@@ -3,8 +3,8 @@ import sqlite3
 connection = sqlite3.connect('database.db')
 
 
-with open('schema.sql') as f:
-    connection.executescript(f.read())
+# with open('schema.sql') as f:
+#     connection.executescript(f.read())
 
 cur = connection.cursor()
 
